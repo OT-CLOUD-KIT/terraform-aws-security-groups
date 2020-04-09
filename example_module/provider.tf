aefa462d96de672d08b3e1ba7389f38a0c10f287
shatrujeet <shatrujeet.sah@opstree.com>
2020-04-10 00:28:03 +0530

    minor update

--- example_module/provider.tf
+++
@@ -1,9 +0,0 @@
-################### Terraform settings ####################
-terraform {
-  required_version = ">= v0.12.0"
-}
-
-################### Provider settings #####################
-provider "aws" {
-  region = "us-east-1"
-}
