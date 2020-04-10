3c87d98e31e1e5cc380d3f5d61f6f7c210ee1825
shatrujeet <shatrujeet.sah@opstree.com>
2020-04-10 15:10:19 +0530

    updated variables

--- main.tf
+++ main.tf
@@ -6,36 +6,40 @@ resource "aws_security_group" "security_group" {
         Environment = var.tag_env_value
     }
 }
+
 resource "aws_security_group_rule" "with_cidr_blocks" {
     count                            = var.enable_whitelist_ip ? length(var.ingress_rule.rules.rule_list) : 0
-    type                             = "ingress"
+    type                             = var.type_ingress_rule
     from_port                        = var.ingress_rule.rules.rule_list[count.index].from_port
     to_port                          = var.ingress_rule.rules.rule_list[count.index].to_port
     protocol                         = var.ingress_rule.rules.rule_list[count.index].protocol
     cidr_blocks                      = var.ingress_rule.rules.rule_list[count.index].cidr
     security_group_id                = aws_security_group.security_group.id
 }
+
 resource "aws_security_group_rule" "with_source_sg" {
     count                            = var.enable_source_security_group_entry ? length(var.ingress_rule.rules.rule_list) : 0
-    type                             = "ingress"
+    type                             = var.type_ingress_rule
     from_port                        = var.ingress_rule.rules.rule_list[count.index].from_port
     to_port                          = var.ingress_rule.rules.rule_list[count.index].to_port
     protocol                         = var.ingress_rule.rules.rule_list[count.index].protocol
     source_security_group_id         = var.ingress_rule.rules.rule_list[count.index].source_SG_ID
     security_group_id                = aws_security_group.security_group.id
 }
+
 resource "aws_security_group_rule" "sg_egress_with_cidr" {
     count                            = length(var.egress_rule.rules.rule_list)
-    type                             = "egress"
+    type                             = var.type_egress_rule
     from_port                        = var.egress_rule.rules.rule_list[count.index].from_port
     to_port                          = var.egress_rule.rules.rule_list[count.index].to_port
     protocol                         = var.egress_rule.rules.rule_list[count.index].protocol
     cidr_blocks                      = var.egress_rule.rules.rule_list[count.index].cidr
     security_group_id                = aws_security_group.security_group.id
 }
+
 resource "aws_security_group_rule" "sg_egress_with_src_sg_id" {
     count                            = var.create_outbound_rule_with_src_sg_id ? length(var.egress_rule.rules.rule_list) : 0
-    type                             = "egress"
+    type                             = var.type_egress_rule
     from_port                        = var.egress_rule.rules.rule_list[count.index].from_port
     to_port                          = var.egress_rule.rules.rule_list[count.index].to_port
     protocol                         = var.egress_rule.rules.rule_list[count.index].protocol
--- vars.tf
+++ vars.tf
@@ -3,36 +3,55 @@ variable "name_sg" {
     type                            = string
     default                         = ""
 }
+
 variable "vpc_id" {
     description                     = "The VPC id"
     type                            = string
     default                         = ""
 }
+
 variable "tag_name_value" {
-    description = "The value of tag Name"
+    description = "The value of tag-key Name"
     type = string
     default = ""
 }
+
 variable "tag_env_value" {
     type = string
-    description = "The value of tag Name"
+    description = "The value of tag-key Environment"
     default = ""
 }
+
+variable "type_ingress_rule" {
+  description = "To select the rule as ingress"
+  type = string
+  default = "ingress"
+}
+
+variable "type_egress_rule" {
+  description = "To select the rule as egress"
+  type = string
+  default = "egress"
+}
+
 variable "enable_whitelist_ip" {
   description = "Do you want to whitelist your Ip/cidr-range"
   type = bool
   default = false
 }
+
 variable "enable_source_security_group_entry" {
   description = "Do you want to use source security group instead of Ip/cidr-range"
   type = bool
   default = false
 }
+
 variable "create_outbound_rule_with_src_sg_id" {
     description = "Do you want to create outbound rules"
     type = bool
     default = false
 }
+
 variable "ingress_rule" {
     type = object({
         rules = object({
