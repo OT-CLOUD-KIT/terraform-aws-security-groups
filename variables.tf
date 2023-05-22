822ef2cf732399cb696e4b7734522819769f7c68
Ashutosh Yadav <ashutosh.yadav@opstree.com>
2023-05-22 18:58:36 +0530

    removing unnecessary variables

--- variables.tf
+++ variables.tf
@@ -22,39 +22,6 @@ variable "vpc_id" {
   default     = ""
 }
 
-variable "sg_ingress_rules" {
-  type = list(object({
-    description  = string
-    from_port    = number
-    to_port      = number
-    protocol     = string
-    cidr         = list(string)
-    ipv6_cidr    = list(string)
-    source_SG_ID = string
-  }))
-  default = [
-    {
-      description  = "opening port 80 for everyone"
-      from_port    = 80
-      to_port      = 80
-      protocol     = "tcp"
-      cidr         = ["0.0.0.0/0"]
-      ipv6_cidr    = []
-      source_SG_ID = ""
-    },
-    {
-      description  = "opening port 443 for everyone"
-      from_port    = 443
-      to_port      = 443
-      protocol     = "tcp"
-      cidr         = ["0.0.0.0/0"]
-      ipv6_cidr    = []
-      source_SG_ID = ""
-    }
-  ]
-}
-
-
 variable "ingress_rule" {
   type = list(object({
     description  = string
